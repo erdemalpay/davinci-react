fc76c251fe64fd150721102b6ec8d5f962640d2e
Erdem Alpay <erdemalpay@gmail.com>
2023-02-18 00:56:16 +0300

    Migrate from cra to vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 const plugin = require("tailwindcss/plugin");
 const withMT = require("@material-tailwind/react/utils/withMT");
 
+/** @type {import('tailwindcss').Config} */
+
 module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
