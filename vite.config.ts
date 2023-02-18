017b88786e554a536ccf014c3744f694cb2898ca
Erdem Alpay <erdemalpay@gmail.com>
2023-02-18 22:11:07 +0300

    Update port

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3001,
+  },
   plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
 });
