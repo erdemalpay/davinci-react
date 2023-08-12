3ae45a4d5ec944ccc7ac8dacd7d53e89ce31d148
Erdem Alpay <erdemalpay@gmail.com>
2023-08-12 22:35:54 +0300

    Prevent editing old tables

--- src/components/common/CardAction.tsx
+++ src/components/common/CardAction.tsx
@@ -1,12 +1,23 @@
+import { useDateContext } from "../../context/Date.context";
+import { isToday } from "../../utils/dateUtil";
+
 interface CardActionProps {
   onClick: () => void;
   IconComponent: React.ComponentType<{ className?: string }>;
 }
 
 export function CardAction({ onClick, IconComponent }: CardActionProps) {
+  const { selectedDate } = useDateContext();
   return (
-    <button onClick={onClick} className="focus:outline-none">
-      <IconComponent className="h-6 w-6" />
-    </button>
+    selectedDate &&
+    isToday(selectedDate) && (
+      <button
+        onClick={onClick}
+        className="focus:outline-none"
+        disabled={!selectedDate || !isToday(selectedDate)}
+      >
+        <IconComponent className="h-6 w-6" />
+      </button>
+    )
   );
 }
