c433f2aa024431d65223eb3615e80dab150c419f
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-04-18 08:59:50 -0500

    Single Count Archieve page is done

---
+++ src/pages/SingleCountArchieve.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useState } from "react";
+import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
+import { Header } from "../components/header/Header";
+import GenericTable from "../components/panelComponents/Tables/GenericTable";
+import { AccountUnit, User } from "../types";
+import { useGetAccountCounts } from "../utils/api/account/count";
+import { useGetAccountProducts } from "../utils/api/account/product";
+import { formatAsLocalDate } from "../utils/format";
+
+const SingleCountArchieve = () => {
+  const { t } = useTranslation();
+  const { archiveId } = useParams();
+  const [tableKey, setTableKey] = useState(0);
+  const counts = useGetAccountCounts();
+  const products = useGetAccountProducts();
+  const [rows, setRows] = useState(
+    counts
+      ?.find((count) => count._id === archiveId)
+      ?.products?.map((option) => ({
+        product: products?.find((item) => item._id === option.product)?.name,
+        unit: (
+          products?.find((item) => item._id === option.product)
+            ?.unit as AccountUnit
+        )?.name,
+        stockQuantity: option.stockQuantity,
+        countQuantity: option.countQuantity,
+      })) || []
+  );
+  const columns = [
+    { key: t("Product"), isSortable: true },
+    { key: t("Unit"), isSortable: true },
+    { key: t("Stock Quantity"), isSortable: true },
+    { key: t("Count Quantity"), isSortable: true },
+  ];
+  const rowKeys = [
+    { key: "product" },
+    { key: "unit" },
+    { key: "stockQuantity" },
+    { key: "countQuantity" },
+  ];
+  useEffect(() => {
+    setRows(
+      counts
+        ?.find((count) => count._id === archiveId)
+        ?.products?.map((option) => ({
+          product: products?.find((item) => item._id === option.product)?.name,
+          unit: (
+            products?.find((item) => item._id === option.product)
+              ?.unit as AccountUnit
+          )?.name,
+          stockQuantity: option.stockQuantity,
+          countQuantity: option.countQuantity,
+        })) || []
+    );
+    setTableKey((prev) => prev + 1);
+  }, [counts, products, archiveId]);
+  const foundCount = counts?.find((count) => count._id === archiveId);
+  return (
+    <>
+      <Header />
+      <div className="w-[95%] mx-auto my-10 ">
+        <GenericTable
+          key={tableKey}
+          rowKeys={rowKeys}
+          columns={columns}
+          rows={rows}
+          title={`${(foundCount?.user as User)?.name} ${formatAsLocalDate(
+            foundCount?.date ?? ""
+          )} ${t("Countu")}`}
+        />
+      </div>
+    </>
+  );
+};
+
+export default SingleCountArchieve;
