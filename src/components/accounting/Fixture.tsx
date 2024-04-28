a9db967737848e9bef09d4d00b275b035074a452
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-04-27 20:10:22 -0500

    Switch Button replaced with switches in the pages

--- src/components/accounting/Fixture.tsx
+++ src/components/accounting/Fixture.tsx
@@ -1,4 +1,3 @@
-import { Switch } from "@headlessui/react";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { FiEdit } from "react-icons/fi";
@@ -19,6 +18,7 @@ import {
   VendorInput,
 } from "../../utils/panelInputs";
 import { ConfirmationDialog } from "../common/ConfirmationDialog";
+import SwitchButton from "../panelComponents/common/SwitchButton";
 import GenericAddEditPanel from "../panelComponents/FormElements/GenericAddEditPanel";
 import { FormKeyTypeEnum } from "../panelComponents/shared/types";
 import GenericTable from "../panelComponents/Tables/GenericTable";
@@ -288,20 +288,7 @@ const Fixture = () => {
     {
       label: t("Show Filters"),
       isUpperSide: true,
-      node: (
-        <Switch
-          checked={showFilters}
-          onChange={() => setShowFilters((value) => !value)}
-          className={`${showFilters ? "bg-green-500" : "bg-red-500"}
-          relative inline-flex h-[20px] w-[36px] min-w-[36px] border-[1px] cursor-pointer rounded-full border-transparent transition-colors duration-200 ease-in-out focus:outline-none`}
-        >
-          <span
-            aria-hidden="true"
-            className={`${showFilters ? "translate-x-4" : "translate-x-0"}
-            pointer-events-none inline-block h-[18px] w-[18px] transform rounded-full bg-white transition duration-200 ease-in-out`}
-          />
-        </Switch>
-      ),
+      node: <SwitchButton checked={showFilters} onChange={setShowFilters} />,
     },
   ];
   const filterPanel = {
