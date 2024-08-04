95e8c31b89917fd0846f199e4c8fe595cde9e6d1
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-08-04 09:17:00 -0500

    fix order payment modal small screen view

--- src/components/orders/orderPayment/OrderPaymentModal.tsx
+++ src/components/orders/orderPayment/OrderPaymentModal.tsx
@@ -81,72 +81,70 @@ const OrderPaymentModal = ({ close, table }: Props) => {
   return (
     <div
       id="popup"
-      className="z-[99999] fixed w-full flex justify-center inset-0"
+      className="z-[99999] fixed w-full h-full inset-0 flex justify-center items-center"
     >
       <div
         onClick={close}
-        className="w-full h-full bg-gray-900 bg-opacity-50 z-0 absolute inset-0"
+        className="w-full h-full bg-gray-900 bg-opacity-50 absolute inset-0"
       />
-      <div className="mx-auto container">
-        <div className="flex items-center justify-center h-full w-full">
-          <div className="bg-white rounded-md shadow fixed overflow-y-auto sm:h-auto w-10/12 lg:w-4/5">
-            <div className="flex flex-col gap-4 border border-gray-200  rounded-lg pb-3 __className_a182b8">
-              {/* header& buttons */}
-              <div className="flex flex-row justify-between items-center px-4 bg-blue-gray-50 rounded-t-lg py-1">
-                {/* header */}
-                <div className="flex flex-col gap-1  ">
-                  <h1 className="font-medium">
-                    <span className="font-semibold">{t("Table")}</span>:
-                    {table.name}
-                  </h1>
-                  <h1 className="font-medium">{user.name}</h1>
-                </div>
-                {/* buttons */}
-                {buttons?.map((button) => {
-                  if (button.isActive) {
-                    return (
-                      <button
-                        key={button.label}
-                        onClick={button.onClick}
-                        className=" w-fit ml-auto bg-gray-200 px-4 py-2 rounded-lg shadow-md  focus:outline-none  hover:bg-gray-300 text-red-300 hover:text-red-500 font-semibold "
-                      >
-                        {button.label}
-                      </button>
-                    );
-                  }
-                })}
+      <div className="relative w-5/6 lg:w-4/5 max-h-full overflow-y-auto">
+        <div className="bg-white rounded-md shadow overflow-y-auto max-h-full">
+          <div className="flex flex-col gap-4 border border-gray-200 rounded-lg pb-3 __className_a182b8">
+            {/* header & buttons */}
+            <div className="flex flex-row justify-between items-center px-4 bg-blue-gray-50 rounded-t-lg py-1">
+              {/* header */}
+              <div className="flex flex-col gap-1">
+                <h1 className="font-medium">
+                  <span className="font-semibold">{t("Table")}</span>:{" "}
+                  {table.name}
+                </h1>
+                <h1 className="font-medium">{user.name}</h1>
               </div>
-              {/* payment part */}
-              <div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-4 py-2 overflow-scroll no-scroll ">
-                <OrderLists
-                  tableOrders={tableOrders}
-                  collectionsTotalAmount={collectionsTotalAmount}
-                />
-                <OrderTotal
-                  tableOrders={tableOrders}
-                  table={table}
-                  collectionsTotalAmount={collectionsTotalAmount}
-                />
-                <OrderPaymentTypes
-                  table={table}
-                  tableOrders={tableOrders}
-                  collectionsTotalAmount={collectionsTotalAmount}
-                />
-              </div>
+              {/* buttons */}
+              {buttons?.map((button) => {
+                if (button.isActive) {
+                  return (
+                    <button
+                      key={button.label}
+                      onClick={button.onClick}
+                      className="w-fit ml-auto bg-gray-200 px-4 py-2 rounded-lg shadow-md focus:outline-none hover:bg-gray-300 text-red-300 hover:text-red-500 font-semibold"
+                    >
+                      {button.label}
+                    </button>
+                  );
+                }
+              })}
             </div>
-            <ConfirmationDialog
-              isOpen={isCloseConfirmationDialogOpen}
-              close={() => {
-                setIsCloseConfirmationDialogOpen(false);
-              }}
-              confirm={() => {
-                finishTable();
-                close();
-              }}
-              title={t("Close Table")}
-              text={t("Table is being closed. Are you sure?")}
-            />
+            {/* payment part */}
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-4 py-2">
+              <OrderLists
+                tableOrders={tableOrders}
+                collectionsTotalAmount={collectionsTotalAmount}
+              />
+              <OrderTotal
+                tableOrders={tableOrders}
+                table={table}
+                collectionsTotalAmount={collectionsTotalAmount}
+              />
+              <OrderPaymentTypes
+                table={table}
+                tableOrders={tableOrders}
+                collectionsTotalAmount={collectionsTotalAmount}
+              />
+            </div>
           </div>
+          <ConfirmationDialog
+            isOpen={isCloseConfirmationDialogOpen}
+            close={() => {
+              setIsCloseConfirmationDialogOpen(false);
+            }}
+            confirm={() => {
+              finishTable();
+              close();
+            }}
+            title={t("Close Table")}
+            text={t("Table is being closed. Are you sure?")}
+          />
         </div>
       </div>
     </div>
