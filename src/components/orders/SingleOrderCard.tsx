d1324a535bb21753033320a013b73ed618ab7895
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-08-07 08:45:34 -0500

    fix single order card css

--- src/components/orders/SingleOrderCard.tsx
+++ src/components/orders/SingleOrderCard.tsx
@@ -27,10 +27,10 @@ const SingleOrderCard = ({ order, user }: Props) => {
   };
 
   return (
-    <div className="flex flex-col justify-between border border-gray-200 rounded-lg bg-white shadow-sm  h-24 __className_a182b8">
-      <div className="flex flex-row gap-4  px-2 mt-1  overflow-scroll no-scrollbar ">
+    <div className="flex flex-col justify-between border border-gray-200 rounded-lg bg-white shadow-sm  max-h-24 __className_a182b8  overflow-scroll no-scrollbar">
+      <div className="flex flex-row gap-4  px-2 mt-1  ">
         {/* img & time */}
-        <div className="flex flex-col gap-1 h-full  items-center">
+        <div className="flex flex-col gap-1 h-16  items-center ">
           {order.status !== OrderStatus.SERVED && (
             <Timer createdAt={timerSetter() ?? new Date()} />
           )}
@@ -41,7 +41,7 @@ const SingleOrderCard = ({ order, user }: Props) => {
           />
         </div>
         {/* itemName,quantity & orderNote */}
-        <div className="flex flex-col gap-2 justify-center  items-center w-full h-full  ">
+        <div className="flex flex-col gap-2 justify-center  items-center w-full h-full  overflow-scroll no-scrollbar  ">
           <div className="flex flex-row justify-between w-full pr-2 items-center ">
             <p>{(order.item as MenuItem)?.name}</p>
             <p>
@@ -53,7 +53,7 @@ const SingleOrderCard = ({ order, user }: Props) => {
         </div>
       </div>
       {/* buttons */}
-      <div className="  flex flex-row justify-between  px-2 ml-auto overflow-hidden mt-[-0.2rem] ">
+      <div className="  flex flex-row justify-between  px-2 ml-auto  ">
         {/* pending ready button */}
         {order.status === OrderStatus.PENDING && (
           <button
