98b4b4af3f52cb5b50cfa4ed060d502c4159747e
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-08-08 09:25:25 -0500

    count archieve filter is added

--- src/components/countLists/productCountList/CountArchive.tsx
+++ src/components/countLists/productCountList/CountArchive.tsx
@@ -12,18 +12,38 @@ import {
   User,
 } from "../../../types";
 import { useGetAccountCounts } from "../../../utils/api/account/count";
+import { useGetAccountStockLocations } from "../../../utils/api/account/stockLocation";
+import { useGetUsers } from "../../../utils/api/user";
 import { formatAsLocalDate } from "../../../utils/format";
+import { StockLocationInput } from "../../../utils/panelInputs";
+import { passesFilter } from "../../../utils/passesFilter";
 import GenericTable from "../../panelComponents/Tables/GenericTable";
+import SwitchButton from "../../panelComponents/common/SwitchButton";
+import { InputTypes } from "../../panelComponents/shared/types";
 
+type FormElementsState = {
+  [key: string]: any;
+};
 const CountArchive = () => {
   const { t } = useTranslation();
+  const [showFilters, setShowFilters] = useState(false);
   const navigate = useNavigate();
   const counts = useGetAccountCounts();
+  const users = useGetUsers();
+  const locations = useGetAccountStockLocations();
   const { setCurrentPage, setSearchQuery, setSortConfigKey } =
     useGeneralContext();
   const pad = (num: number) => (num < 10 ? `0${num}` : num);
   const { user } = useUserContext();
   const [tableKey, setTableKey] = useState(0);
+  const [filterPanelFormElements, setFilterPanelFormElements] =
+    useState<FormElementsState>({
+      createdBy: "",
+      countList: "",
+      location: "",
+      after: "",
+      before: "",
+    });
   const allRows = counts
     .filter((count) => {
       if (
@@ -47,8 +67,11 @@ const CountArchive = () => {
       return {
         ...count,
         cntLst: (count.countList as AccountCountList).name,
+        cntLstId: (count.countList as AccountCountList)._id,
         lctn: (count.location as AccountStockLocation).name,
+        lctnId: (count.location as AccountStockLocation)._id,
         usr: (count.user as User)?.name,
+        usrId: (count.user as User)?._id,
         startDate: format(count.createdAt, "yyyy-MM-dd"),
         formattedStartDate: formatAsLocalDate(
           format(count.createdAt, "yyyy-MM-dd")
@@ -150,10 +173,80 @@ const CountArchive = () => {
       },
     },
   ];
+  const filterPanelInputs = [
+    {
+      type: InputTypes.SELECT,
+      formKey: "createdBy",
+      label: t("Created By"),
+      options: users
+        .filter((user) => user.active)
+        .map((user) => ({
+          value: user._id,
+          label: user.name,
+        })),
+      placeholder: t("Created By"),
+      required: true,
+    },
+
+    StockLocationInput({ locations: locations, required: true }),
+    {
+      type: InputTypes.SELECT,
+      formKey: "countList",
+      label: t("NounCount"),
+      options: counts.map((count) => ({
+        value: (count.countList as AccountCountList)._id,
+        label: t((count.countList as AccountCountList).name),
+      })),
+      placeholder: t("NounCount"),
+      required: true,
+    },
+
+    {
+      type: InputTypes.DATE,
+      formKey: "after",
+      label: t("Start Date"),
+      placeholder: t("Start Date"),
+      required: true,
+      isDatePicker: true,
+    },
+    {
+      type: InputTypes.DATE,
+      formKey: "before",
+      label: t("End Date"),
+      placeholder: t("End Date"),
+      required: true,
+      isDatePicker: true,
+    },
+  ];
+  const filterPanel = {
+    isFilterPanelActive: showFilters,
+    inputs: filterPanelInputs,
+    formElements: filterPanelFormElements,
+    setFormElements: setFilterPanelFormElements,
+    closeFilters: () => setShowFilters(false),
+  };
+  const filters = [
+    {
+      label: t("Show Filters"),
+      isUpperSide: true,
+      node: <SwitchButton checked={showFilters} onChange={setShowFilters} />,
+    },
+  ];
   useEffect(() => {
-    setRows(allRows);
+    const filteredRows = allRows.filter((row) => {
+      return (
+        (filterPanelFormElements.before === "" ||
+          row.startDate <= filterPanelFormElements.before) &&
+        (filterPanelFormElements.after === "" ||
+          row.startDate >= filterPanelFormElements.after) &&
+        passesFilter(filterPanelFormElements.location, row.lctnId) &&
+        passesFilter(filterPanelFormElements.countList, row.cntLstId) &&
+        passesFilter(filterPanelFormElements.createdBy, row.usrId)
+      );
+    });
+    setRows(filteredRows);
     setTableKey((prev) => prev + 1);
-  }, [counts, user]);
+  }, [counts, user, locations, users, filterPanelFormElements]);
 
   return (
     <>
@@ -165,6 +258,8 @@ const CountArchive = () => {
           columns={columns}
           rows={rows}
           title={t("Count Archive")}
+          filterPanel={filterPanel}
+          filters={filters}
         />
       </div>
     </>
