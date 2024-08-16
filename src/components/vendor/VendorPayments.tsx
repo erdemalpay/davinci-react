f0d201ddab7b7351f09a23767892b10af712c7d9
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-08-16 08:40:57 -0500

    vendor payments filters are added and vender expenses page is renewed

--- src/components/vendor/VendorPayments.tsx
+++ src/components/vendor/VendorPayments.tsx
@@ -3,18 +3,36 @@ import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
 import { AccountPaymentMethod, AccountStockLocation } from "../../types";
 import { useGetAccountPayments } from "../../utils/api/account/payment";
+import { useGetAccountPaymentMethods } from "../../utils/api/account/paymentMethod";
+import { useGetAccountStockLocations } from "../../utils/api/account/stockLocation";
 import { useGetAccountVendors } from "../../utils/api/account/vendor";
 import { formatAsLocalDate } from "../../utils/format";
+import { StockLocationInput } from "../../utils/panelInputs";
+import { passesFilter } from "../../utils/passesFilter";
 import GenericTable from "../panelComponents/Tables/GenericTable";
+import SwitchButton from "../panelComponents/common/SwitchButton";
+import { InputTypes } from "../panelComponents/shared/types";
 
+type FormElementsState = {
+  [key: string]: any;
+};
 const VendorPayments = () => {
   const { t } = useTranslation();
   const { vendorId } = useParams();
   const vendors = useGetAccountVendors();
+  const locations = useGetAccountStockLocations();
+  const paymentMethods = useGetAccountPaymentMethods();
   const selectedVendor = vendors?.find((item) => item._id === vendorId);
   if (!selectedVendor) return <></>;
   const payments = useGetAccountPayments();
   const [tableKey, setTableKey] = useState(0);
+  const [filterPanelFormElements, setFilterPanelFormElements] =
+    useState<FormElementsState>({
+      location: "",
+      paymentMethod: "",
+      before: "",
+      after: "",
+    });
   const allRows = payments
     ?.filter((i) => i?.vendor?._id === selectedVendor?._id)
     ?.map((payment) => {
@@ -22,10 +40,13 @@ const VendorPayments = () => {
         ...payment,
         formattedDate: formatAsLocalDate(payment?.date),
         usr: payment?.user?.name,
-        pymntMthd: (payment?.paymentMethod as AccountPaymentMethod)?.name,
+        pymntMthd: t((payment?.paymentMethod as AccountPaymentMethod)?.name),
+        pymntMthdId: (payment?.paymentMethod as AccountPaymentMethod)?._id,
         lctn: (payment?.location as AccountStockLocation)?.name,
+        lctnId: (payment?.location as AccountStockLocation)?._id,
       };
     });
+  const [showFilters, setShowFilters] = useState(false);
   const [rows, setRows] = useState(allRows);
   const columns = [
     { key: "ID", isSortable: true },
@@ -57,10 +78,75 @@ const VendorPayments = () => {
     { key: "serviceInvoice" },
   ];
 
+  const filterPanelInputs = [
+    StockLocationInput({ locations: locations, required: true }),
+    {
+      type: InputTypes.SELECT,
+      formKey: "paymentMethod",
+      label: t("Payment Method"),
+      options: paymentMethods.map((paymentMethod) => ({
+        value: paymentMethod._id,
+        label: t(paymentMethod.name),
+      })),
+      placeholder: t("Payment Method"),
+      required: true,
+    },
+
+    {
+      type: InputTypes.DATE,
+      formKey: "after",
+      label: t("Start Date"),
+      placeholder: t("Start Date"),
+      required: true,
+      isDatePicker: true,
+    },
+    {
+      type: InputTypes.DATE,
+      formKey: "before",
+      label: t("End Date"),
+      placeholder: t("End Date"),
+      required: true,
+      isDatePicker: true,
+    },
+  ];
+  const filterPanel = {
+    isFilterPanelActive: showFilters,
+    inputs: filterPanelInputs,
+    formElements: filterPanelFormElements,
+    setFormElements: setFilterPanelFormElements,
+    closeFilters: () => setShowFilters(false),
+  };
+  const filters = [
+    {
+      label: t("Show Filters"),
+      isUpperSide: true,
+      node: <SwitchButton checked={showFilters} onChange={setShowFilters} />,
+    },
+  ];
+
   useEffect(() => {
-    setRows(allRows);
+    const filteredRows = allRows.filter((row) => {
+      if (!row?.date) {
+        return false;
+      }
+      return (
+        (filterPanelFormElements.before === "" ||
+          row.date <= filterPanelFormElements.before) &&
+        (filterPanelFormElements.after === "" ||
+          row.date >= filterPanelFormElements.after) &&
+        passesFilter(filterPanelFormElements.location, row.lctnId) &&
+        passesFilter(filterPanelFormElements.paymentMethod, row.pymntMthdId)
+      );
+    });
+    setRows(filteredRows);
     setTableKey((prev) => prev + 1);
-  }, [payments, selectedVendor]);
+  }, [
+    payments,
+    selectedVendor,
+    locations,
+    paymentMethods,
+    filterPanelFormElements,
+  ]);
 
   return (
     <div className="w-[95%] mx-auto ">
@@ -70,6 +156,8 @@ const VendorPayments = () => {
         columns={columns}
         isActionsActive={false}
         rows={rows}
+        filterPanel={filterPanel}
+        filters={filters}
         title={t("Vendor Payments")}
         isSearch={false}
       />
