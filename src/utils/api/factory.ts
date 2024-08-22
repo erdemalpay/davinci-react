0f5f392c0fd7298c33478136751a51999cf5ab41
Erdem Alpay <erdemalpay@gmail.com>
2024-08-22 08:32:52 +0300

    Revert stale time infinities

--- src/utils/api/factory.ts
+++ src/utils/api/factory.ts
@@ -45,9 +45,7 @@ interface Props<T> {
 export function useGet<T>(path: string, queryKey?: QueryKey) {
   // We are using path as a query key if queryKey is not provided
   const fetchQueryKey = queryKey || [path];
-  const { data } = useQuery(fetchQueryKey, () => get<T>({ path }), {
-    staleTime: Infinity,
-  });
+  const { data } = useQuery(fetchQueryKey, () => get<T>({ path }));
   return data;
 }
 
