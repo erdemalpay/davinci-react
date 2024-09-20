07624e3770b68f088576925f33ae48109df71435
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-09-20 12:35:19 +0300

    is mutating loading will be visible on payment modal

--- src/components/orders/orderPayment/OrderPaymentModal.tsx
+++ src/components/orders/orderPayment/OrderPaymentModal.tsx
@@ -12,6 +12,7 @@ import {
   Table,
 } from "../../../types";
 
+import { useIsMutating } from "@tanstack/react-query";
 import {
   useCloseTableMutation,
   useReopenTableMutation,
@@ -35,6 +36,7 @@ type ButtonType = {
 const OrderPaymentModal = ({ close, table, orders, collections }: Props) => {
   const { t } = useTranslation();
   const { user } = useUserContext();
+  const isMutating = useIsMutating();
   const { mutate: reopenTable } = useReopenTableMutation();
   const [isCloseConfirmationDialogOpen, setIsCloseConfirmationDialogOpen] =
     useState(false);
@@ -105,6 +107,36 @@ const OrderPaymentModal = ({ close, table, orders, collections }: Props) => {
         onClick={close}
         className="w-full h-full bg-gray-900 bg-opacity-50 absolute inset-0"
       />
+      {isMutating ? (
+        <div className="fixed inset-0 w-full h-full z-50">
+          -
+          <div className="absolute inset-0 w-full h-full z-50 opacity-50 bg-black text-white">
+            <div className="flex justify-center w-full h-full items-center">
+              <svg
+                className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                ></circle>
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg>
+              <h1 className="text-2xl">Loading...</h1>
+            </div>
+          </div>
+        </div>
+      ) : null}
       <div className="relative w-6/7 lg:w-4/5 max-h-full overflow-y-auto">
         <div className="bg-white rounded-md shadow overflow-y-auto max-h-full">
           <div className="border border-gray-200 rounded-lg pb-3 __className_a182b8">
