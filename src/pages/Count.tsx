1431f4fede3afe9e81c8d241ea53fe931d0fc9e5
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-10-24 15:33:24 -0500

    while counting product dynamicaly can be added into countlist

--- src/pages/Count.tsx
+++ src/pages/Count.tsx
@@ -2,8 +2,13 @@ import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useNavigate, useParams } from "react-router-dom";
 import { Header } from "../components/header/Header";
+import GenericAddEditPanel from "../components/panelComponents/FormElements/GenericAddEditPanel";
 import TextInput from "../components/panelComponents/FormElements/TextInput";
 import PageNavigator from "../components/panelComponents/PageNavigator/PageNavigator";
+import {
+  FormKeyTypeEnum,
+  InputTypes,
+} from "../components/panelComponents/shared/types";
 import GenericTable from "../components/panelComponents/Tables/GenericTable";
 import { H5 } from "../components/panelComponents/Typography";
 import { useGeneralContext } from "../context/General.context";
@@ -13,7 +18,10 @@ import {
   useAccountCountMutations,
   useGetAccountCounts,
 } from "../utils/api/account/count";
-import { useGetAccountCountLists } from "../utils/api/account/countList";
+import {
+  useAccountCountListMutations,
+  useGetAccountCountLists,
+} from "../utils/api/account/countList";
 import { useGetAccountProducts } from "../utils/api/account/product";
 import { useGetAccountStocks } from "../utils/api/account/stock";
 
@@ -26,6 +34,8 @@ const Count = () => {
   const stocks = useGetAccountStocks();
   const { updateAccountCount } = useAccountCountMutations();
   const countLists = useGetAccountCountLists();
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const { updateAccountCountList } = useAccountCountListMutations();
   const [tableKey, setTableKey] = useState(0);
   const {
     setCurrentPage,
@@ -34,6 +44,9 @@ const Count = () => {
     setSortConfigKey,
   } = useGeneralContext();
   const { location, countListId } = useParams();
+  const [form, setForm] = useState({
+    product: [],
+  });
   const [rows, setRows] = useState(
     countLists
       ?.find((cl) => cl?._id === countListId)
@@ -80,50 +93,78 @@ const Count = () => {
       canBeClicked: false,
     },
   ];
-  useEffect(() => {
-    setRows(
-      countLists
-        .find((cl) => cl._id === countListId)
-        ?.products?.map((countListProduct) => {
-          const currentCount = counts?.find((item) => {
-            return (
-              item.isCompleted === false &&
-              item.location === location &&
-              item.user === user?._id &&
-              item.countList === countListId
-            );
-          });
-          if (location && countListProduct.locations.includes(location)) {
-            return {
-              products: currentCount?.products,
-              productId: countListProduct.product,
-              product:
-                products?.find((p) => p?._id === countListProduct.product)
-                  ?.name || "",
-              countQuantity: currentCount?.products?.find(
-                (countProduct) =>
-                  countProduct.product === countListProduct.product
-              )?.countQuantity,
-            };
-          }
-          return { product: "", countQuantity: 0 };
+  const addProductInputs = [
+    {
+      type: InputTypes.SELECT,
+      formKey: "product",
+      label: t("Product"),
+      options: products
+        .filter((item) => {
+          const countList = countLists.find((item) => item._id === countListId);
+          return !countList?.products?.some((pro) => pro.product === item._id);
         })
-        .filter((item) => item.product !== "") || []
-    );
-    setTableKey((prev) => prev + 1);
-  }, [
-    countListId,
-    countLists,
-    location,
-    products,
-    stocks,
-    counts,
-    i18n.language,
-  ]);
+        .map((product) => {
+          return {
+            value: product._id,
+            label: product.name,
+          };
+        }),
+      isMultiple: true,
+      placeholder: t("Product"),
+      required: true,
+    },
+  ];
+  const addProductFormKeys = [{ key: "product", type: FormKeyTypeEnum.STRING }];
+
   const columns = [
     { key: t("Product"), isSortable: true },
     { key: t("Quantity"), isSortable: true },
   ];
+  const addButton = {
+    name: t("Add Product"),
+    icon: "",
+    className: "bg-blue-500 hover:text-blue-500 hover:border-blue-500 ",
+    isModal: true,
+    modal: countListId ? (
+      <GenericAddEditPanel
+        isOpen={isAddModalOpen}
+        close={() => setIsAddModalOpen(false)}
+        inputs={addProductInputs}
+        formKeys={addProductFormKeys}
+        submitItem={updateAccountCountList as any}
+        isEditMode={true}
+        setForm={setForm}
+        topClassName="flex flex-col gap-2 "
+        handleUpdate={() => {
+          const countListProducts = () => {
+            let productRows = [];
+            const products =
+              countLists.find((item) => item._id === countListId)?.products ||
+              [];
+            const newProducts = form.product?.map((item) => ({
+              product: item,
+              locations:
+                countLists.find((item) => item._id === countListId)
+                  ?.locations ?? [],
+            }));
+            productRows = [...products, ...newProducts];
+            return productRows;
+          };
+          updateAccountCountList({
+            id: countListId,
+            updates: {
+              products: countListProducts(),
+            },
+          });
+        }}
+      />
+    ) : (
+      ""
+    ),
+    isModalOpen: isAddModalOpen,
+    setIsModal: setIsAddModalOpen,
+    isPath: false,
+  };
   const rowKeys = [
     { key: "product" },
     {
@@ -191,6 +232,46 @@ const Count = () => {
       },
     },
   ];
+  useEffect(() => {
+    setRows(
+      countLists
+        .find((cl) => cl._id === countListId)
+        ?.products?.map((countListProduct) => {
+          const currentCount = counts?.find((item) => {
+            return (
+              item.isCompleted === false &&
+              item.location === location &&
+              item.user === user?._id &&
+              item.countList === countListId
+            );
+          });
+          if (location && countListProduct.locations.includes(location)) {
+            return {
+              products: currentCount?.products,
+              productId: countListProduct.product,
+              product:
+                products?.find((p) => p?._id === countListProduct.product)
+                  ?.name || "",
+              countQuantity: currentCount?.products?.find(
+                (countProduct) =>
+                  countProduct.product === countListProduct.product
+              )?.countQuantity,
+            };
+          }
+          return { product: "", countQuantity: 0 };
+        })
+        .filter((item) => item.product !== "") || []
+    );
+    setTableKey((prev) => prev + 1);
+  }, [
+    countListId,
+    countLists,
+    location,
+    products,
+    stocks,
+    counts,
+    i18n.language,
+  ]);
   return (
     <>
       <Header />
@@ -203,6 +284,7 @@ const Count = () => {
           rows={rows}
           title={t("Count")}
           isActionsActive={false}
+          addButton={addButton}
         />
         <div className="flex justify-end mt-4">
           <button
