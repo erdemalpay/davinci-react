c26e13053b4164a67c9f1cd7b438768be95747a0
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-11-20 19:50:35 -0600

    product stock history under product tab is fixed

--- src/components/product/ProductStockHistory.tsx
+++ src/components/product/ProductStockHistory.tsx
@@ -1,13 +1,19 @@
+import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/24/outline";
+import { format } from "date-fns";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useParams } from "react-router-dom";
+import { useGeneralContext } from "../../context/General.context";
 import { stockHistoryStatuses } from "../../types";
 import { useGetAccountProducts } from "../../utils/api/account/product";
+import { useGetAccountProductStockHistorys } from "../../utils/api/account/productStockHistory";
 import { useGetAccountStockLocations } from "../../utils/api/account/stockLocation";
 import { useGetUsers } from "../../utils/api/user";
+import { formatAsLocalDate } from "../../utils/format";
+import { getItem } from "../../utils/getItem";
 import { StockLocationInput } from "../../utils/panelInputs";
-import { passesFilter } from "../../utils/passesFilter";
 import GenericTable from "../panelComponents/Tables/GenericTable";
+import { H5 } from "../panelComponents/Typography";
 import SwitchButton from "../panelComponents/common/SwitchButton";
 import { InputTypes } from "../panelComponents/shared/types";
 
@@ -19,44 +25,72 @@ const ProductStockHistory = () => {
   const { t } = useTranslation();
   const { productId } = useParams();
   const products = useGetAccountProducts();
+  const [currentPage, setCurrentPage] = useState(1);
+  const { rowsPerPage } = useGeneralContext();
+  const [usedRowsPerPage, setUsedRowsPerPage] = useState(rowsPerPage);
+
   const selectedProduct = products?.find(
     (product) => product._id === productId
   );
-  const users = useGetUsers();
-  if (!selectedProduct) return <></>;
-  // const stockHistories = useGetAccountProductStockHistorys();
-  const [tableKey, setTableKey] = useState(0);
-  const locations = useGetAccountStockLocations();
-  const [showFilters, setShowFilters] = useState(false);
+  function handleSort(value: string) {
+    if (filterPanelFormElements.sort === value) {
+      setFilterPanelFormElements({
+        ...filterPanelFormElements,
+        asc: filterPanelFormElements.asc === 1 ? -1 : 1,
+      });
+    } else {
+      setFilterPanelFormElements({
+        ...filterPanelFormElements,
+        asc: value === "createdAt" ? 1 : -1,
+        sort: value,
+      });
+    }
+  }
   const [filterPanelFormElements, setFilterPanelFormElements] =
     useState<FormElementsState>({
+      product: selectedProduct?._id,
+      expenseType: "",
       location: "",
       status: "",
       before: "",
       after: "",
+      sort: "",
+      asc: 1,
     });
+  const users = useGetUsers();
+  if (!selectedProduct) return <></>;
+  const stockHistoriesPayload = useGetAccountProductStockHistorys(
+    currentPage,
+    usedRowsPerPage,
+    filterPanelFormElements
+  );
+  // const stockHistories = useGetAccountProductStockHistorys();
+  const [tableKey, setTableKey] = useState(0);
+  const locations = useGetAccountStockLocations();
+  const [showFilters, setShowFilters] = useState(false);
   const pad = (num: number) => (num < 10 ? `0${num}` : num);
-  // const allRows = stockHistories
-  //   .filter((item) => item.product === selectedProduct?._id)
-  //   .map((stockHistory) => {
-  //     if (!stockHistory?.createdAt) {
-  //       return null;
-  //     }
-  //     const date = new Date(stockHistory.createdAt);
-  //     return {
-  //       ...stockHistory,
-  //       prdct: getItem(stockHistory.product, products)?.name,
-  //       lctn: getItem(stockHistory?.location, locations)?.name,
-  //       usr: getItem(stockHistory?.user, users)?.name,
-  //       date: format(stockHistory?.createdAt, "yyyy-MM-dd"),
-  //       formattedDate: formatAsLocalDate(
-  //         format(stockHistory?.createdAt, "yyyy-MM-dd")
-  //       ),
-  //       hour: `${pad(date.getHours())}:${pad(date.getMinutes())}`,
-  //     };
-  //   })
-  //   .filter((item) => item !== null);
-  const allRows: any = [];
+  const allRows = stockHistoriesPayload?.data
+    ?.map((stockHistory) => {
+      if (!stockHistory?.createdAt) {
+        return null;
+      }
+      const date = new Date(stockHistory.createdAt);
+      return {
+        ...stockHistory,
+        prdct: getItem(stockHistory.product, products)?.name,
+        lctn: getItem(stockHistory?.location, locations)?.name,
+        usr: getItem(stockHistory?.user, users)?.name,
+        newQuantity:
+          (stockHistory?.currentAmount ?? 0) + (stockHistory?.change ?? 0),
+        date: format(stockHistory?.createdAt, "yyyy-MM-dd"),
+        formattedDate: formatAsLocalDate(
+          format(stockHistory?.createdAt, "yyyy-MM-dd")
+        ),
+        hour: `${pad(date.getHours())}:${pad(date.getMinutes())}`,
+      };
+    })
+    .filter((item) => item !== null);
+
   const [rows, setRows] = useState(allRows);
   const filterPanelInputs = [
     StockLocationInput({ locations: locations }),
@@ -90,15 +124,37 @@ const ProductStockHistory = () => {
       isDatePicker: true,
     },
   ];
+  const createColumn = (key: string, title: string) => ({
+    key: t(title),
+    isSortable: true,
+    node: () => (
+      <th
+        key={key}
+        className="font-bold text-left cursor-pointer"
+        onClick={() => handleSort(key)}
+      >
+        <div className="flex gap-x-2 pl-3 items-center py-3 min-w-8">
+          <H5>{t(title)}</H5>
+          {filterPanelFormElements.sort === key &&
+            (filterPanelFormElements.asc === 1 ? (
+              <ArrowUpIcon className="h-4 w-4 my-auto" />
+            ) : (
+              <ArrowDownIcon className="h-4 w-4 my-auto" />
+            ))}
+        </div>
+      </th>
+    ),
+  });
   const columns = [
-    { key: t("Date"), isSortable: true },
-    { key: t("Hour"), isSortable: true },
-    { key: t("User"), isSortable: true },
-    { key: t("Product"), isSortable: true },
-    { key: t("Location"), isSortable: true },
-    { key: t("Old Quantity"), isSortable: true },
-    { key: t("Changed"), isSortable: true },
-    { key: t("Status"), isSortable: true },
+    createColumn("createdAt", "Date"),
+    { key: t("Hour"), isSortable: false },
+    createColumn("user", "User"),
+    { key: t("Product"), isSortable: false },
+    createColumn("location", "Location"),
+    { key: t("Old Quantity"), isSortable: false },
+    { key: t("Changed"), isSortable: false },
+    { key: t("New Quantity"), isSortable: false },
+    createColumn("status", "Status"),
   ];
   const rowKeys = [
     {
@@ -133,6 +189,10 @@ const ProductStockHistory = () => {
       className: "min-w-32 pr-1",
     },
     {
+      key: "newQuantity",
+      className: "min-w-32 pr-1",
+    },
+    {
       key: "status",
       className: "min-w-32 pr-1",
       node: (row: any) => {
@@ -151,29 +211,13 @@ const ProductStockHistory = () => {
     },
   ];
   useEffect(() => {
-    const filteredRows = allRows?.filter((stockHistory: any) => {
-      if (!stockHistory?.createdAt) {
-        return false;
-      }
-      return (
-        (filterPanelFormElements.before === "" ||
-          stockHistory?.createdAt <= filterPanelFormElements.before) &&
-        (filterPanelFormElements.after === "" ||
-          stockHistory?.createdAt >= filterPanelFormElements.after) &&
-        passesFilter(filterPanelFormElements.location, stockHistory.location) &&
-        passesFilter(filterPanelFormElements.status, stockHistory.status)
-      );
-    });
-    setRows(filteredRows);
+    setCurrentPage(1);
+  }, [filterPanelFormElements]);
+
+  useEffect(() => {
+    setRows(allRows);
     setTableKey((prev) => prev + 1);
-  }, [
-    // stockHistories,
-    filterPanelFormElements,
-    selectedProduct,
-    locations,
-    users,
-    products,
-  ]);
+  }, [stockHistoriesPayload, users, products, locations]);
 
   const filterPanel = {
     isFilterPanelActive: showFilters,
@@ -181,7 +225,18 @@ const ProductStockHistory = () => {
     formElements: filterPanelFormElements,
     setFormElements: setFilterPanelFormElements,
     closeFilters: () => setShowFilters(false),
+    isApplyButtonActive: true,
   };
+  const pagination = stockHistoriesPayload
+    ? {
+        currentPage: stockHistoriesPayload.page,
+        totalPages: stockHistoriesPayload.totalPages,
+        setCurrentPage: setCurrentPage,
+        rowsPerPage: usedRowsPerPage,
+        setRowsPerPage: setUsedRowsPerPage,
+        totalRows: stockHistoriesPayload.totalNumber,
+      }
+    : null;
   const filters = [
     {
       label: t("Show Filters"),
@@ -196,11 +251,12 @@ const ProductStockHistory = () => {
           key={tableKey}
           rowKeys={rowKeys}
           columns={columns}
-          rows={rows}
+          rows={rows ?? []}
           filterPanel={filterPanel}
           filters={filters}
           title={t("Product Stock History")}
           isActionsActive={false}
+          {...(pagination && { pagination })}
         />
       </div>
     </>
