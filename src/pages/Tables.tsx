5f6a0398866c0e94d2aa0e8a8ede6ad3bbbf07ad
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-11-24 16:51:25 -0600

    some css fix

--- src/pages/Tables.tsx
+++ src/pages/Tables.tsx
@@ -794,7 +794,7 @@ const Tables = () => {
             }
             setOrderForm(initialOrderForm);
           }}
-          generalClassName=" md:rounded-l-none shadow-none mt-[-4rem] md:mt-0"
+          generalClassName="  shadow-none mt-[-4rem] md:mt-0"
           topClassName="flex flex-col gap-2   "
         />
       )}
