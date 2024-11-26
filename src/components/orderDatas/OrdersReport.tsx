b4f55735b4739bd019ac35e853a44792cc667d56
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2024-11-26 10:58:52 -0600

    fix empty paymentmethod for return

--- src/components/orderDatas/OrdersReport.tsx
+++ src/components/orderDatas/OrdersReport.tsx
@@ -447,7 +447,7 @@ const OrdersReport = () => {
             returnOrder({
               orderId: rowToAction._id,
               returnQuantity: returnOrderForm.quantity,
-              paymentMethod: rowToAction.paymentMethod,
+              paymentMethod: rowToAction?.paymentMethod ?? "cash",
             });
           }}
           constantValues={{
