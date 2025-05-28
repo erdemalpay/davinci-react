fc3df5fafdf52d1f5d386ad9980978ceb6052d60
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2025-05-28 15:14:24 -0500

    new order addition css changed

--- src/components/tables/NewOrderListPanel.tsx
+++ src/components/tables/NewOrderListPanel.tsx
@@ -42,31 +42,52 @@ const NewOrderListPanel = () => {
                   : "bg-green-200"
               } `}
             >
-              <div className="flex flex-row justify-between items-center">
-                <div className="flex flex-row gap-2  items-center  ">
-                  {/* decrement */}
-                  <FiMinusCircle
-                    className="w-5 h-5 flex-shrink-0  text-red-500  hover:text-red-800 cursor-pointer focus:outline-none"
+              <div className="flex flex-row gap-1  ">
+                {selectedNewOrders?.includes(index) ? (
+                  <MdOutlineCheckBox
+                    className="my-auto mx-auto text-lg cursor-pointer hover:scale-105 w-fit "
                     onClick={() => {
-                      if (!order.quantity) return;
-                      if (Number(order.quantity) === 1) {
-                        const newOrders = [...orderCreateBulk];
-                        newOrders.splice(index, 1);
-                        setOrderCreateBulk(newOrders);
-                        return;
-                      } else {
-                        const newOrders = [...orderCreateBulk];
-                        newOrders[index].quantity = Number(order.quantity) - 1;
-                        setOrderCreateBulk(newOrders);
-                      }
+                      setSelectedNewOrders(
+                        selectedNewOrders?.filter(
+                          (selectedOrder) => selectedOrder !== index
+                        )
+                      );
                     }}
                   />
-                  {/* name and quantity */}
-                  <div className="flex w-5/6 gap-1 items-center ">
-                    <p>{orderItem?.name}</p>
-                    <h1 className="text-xs">({order.quantity})</h1>
-                  </div>
-                  <div className="flex flex-row gap-1 items-center">
+                ) : (
+                  <MdOutlineCheckBoxOutlineBlank
+                    className="my-auto mx-auto text-lg cursor-pointer hover:scale-105 w-fit "
+                    onClick={() => {
+                      setSelectedNewOrders([...selectedNewOrders, index]);
+                    }}
+                  />
+                )}
+                <div className="flex flex-row justify-between items-center  w-full">
+                  <div className="flex flex-row gap-2  items-center  ">
+                    {/* name and quantity */}
+                    <div className="flex w-5/6 gap-1 items-center ">
+                      <p>{orderItem?.name}</p>
+                      <h1 className="text-xs">({order.quantity})</h1>
+                    </div>
+
+                    {/* decrement */}
+                    <FiMinusCircle
+                      className="w-5 h-5 flex-shrink-0  text-red-500  hover:text-red-800 cursor-pointer focus:outline-none"
+                      onClick={() => {
+                        if (!order.quantity) return;
+                        if (Number(order.quantity) === 1) {
+                          const newOrders = [...orderCreateBulk];
+                          newOrders.splice(index, 1);
+                          setOrderCreateBulk(newOrders);
+                          return;
+                        } else {
+                          const newOrders = [...orderCreateBulk];
+                          newOrders[index].quantity =
+                            Number(order.quantity) - 1;
+                          setOrderCreateBulk(newOrders);
+                        }
+                      }}
+                    />
                     <GoPlusCircle
                       className="w-5 h-5 flex-shrink-0  text-green-500  hover:text-green-800 cursor-pointer focus:outline-none"
                       onClick={() => {
@@ -76,53 +97,34 @@ const NewOrderListPanel = () => {
                         setOrderCreateBulk(newOrders);
                       }}
                     />
-                    {selectedNewOrders?.includes(index) ? (
-                      <MdOutlineCheckBox
-                        className="my-auto mx-auto text-lg cursor-pointer hover:scale-105"
-                        onClick={() => {
-                          setSelectedNewOrders(
-                            selectedNewOrders?.filter(
-                              (selectedOrder) => selectedOrder !== index
-                            )
-                          );
-                        }}
-                      />
-                    ) : (
-                      <MdOutlineCheckBoxOutlineBlank
-                        className="my-auto mx-auto text-lg cursor-pointer hover:scale-105"
-                        onClick={() => {
-                          setSelectedNewOrders([...selectedNewOrders, index]);
-                        }}
-                      />
-                    )}
                   </div>
-                </div>
 
-                <div className="flex flex-row gap-2">
-                  {orderCreateBulk[index].status !== OrderStatus.SERVED && (
-                    <ButtonTooltip content={t("Served")}>
-                      <PiBellSimpleRingingFill
-                        className="text-green-500 cursor-pointer text-lg px-[0.5px]"
-                        onClick={() => {
-                          orderCreateBulk[index] = {
-                            ...orderCreateBulk[index],
-                            status: OrderStatus.SERVED,
-                            deliveredAt: new Date(),
-                            deliveredBy: user?._id,
-                          };
-                          setOrderCreateBulk([...orderCreateBulk]);
-                        }}
-                      />
-                    </ButtonTooltip>
-                  )}
-                  <HiOutlineTrash
-                    className="text-red-400 hover:text-red-700 cursor-pointer text-lg px-[0.5px]"
-                    onClick={() =>
-                      setOrderCreateBulk(
-                        orderCreateBulk.filter((_, i) => i !== index)
-                      )
-                    }
-                  />
+                  <div className="flex flex-row gap-2">
+                    {orderCreateBulk[index].status !== OrderStatus.SERVED && (
+                      <ButtonTooltip content={t("Served")}>
+                        <PiBellSimpleRingingFill
+                          className="text-green-500 cursor-pointer text-lg px-[0.5px]"
+                          onClick={() => {
+                            orderCreateBulk[index] = {
+                              ...orderCreateBulk[index],
+                              status: OrderStatus.SERVED,
+                              deliveredAt: new Date(),
+                              deliveredBy: user?._id,
+                            };
+                            setOrderCreateBulk([...orderCreateBulk]);
+                          }}
+                        />
+                      </ButtonTooltip>
+                    )}
+                    <HiOutlineTrash
+                      className="text-red-400 hover:text-red-700 cursor-pointer text-lg px-[0.5px]"
+                      onClick={() =>
+                        setOrderCreateBulk(
+                          orderCreateBulk.filter((_, i) => i !== index)
+                        )
+                      }
+                    />
+                  </div>
                 </div>
               </div>
             </div>
