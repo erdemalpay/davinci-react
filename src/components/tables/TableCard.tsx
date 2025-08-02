835d83b3d667d48b25c135defe0e7783e69197c0
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2025-08-02 11:57:30 -0500

    activity player selections are added

--- src/components/tables/TableCard.tsx
+++ src/components/tables/TableCard.tsx
@@ -469,11 +469,19 @@ export function TableCard({
         isDisabled: table?.type !== TableTypes.ACTIVITY,
       },
       {
-        type: InputTypes.TEXT,
+        type: InputTypes.TEXTAREA,
         formKey: "activityPlayer",
         label: t("Player Number"),
         placeholder: t("Player Number"),
         required: false,
+        inputClassName: "p-0 h-10",
+        options:
+          tableOrders
+            ?.filter((order) => order?.activityPlayer)
+            ?.map((order) => ({
+              value: order.activityPlayer,
+              label: order.activityPlayer,
+            })) ?? [],
         isDisabled: table?.type !== TableTypes.ACTIVITY,
       },
       {
