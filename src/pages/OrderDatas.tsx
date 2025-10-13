79143a879f6166ac9a59388ed0ba511a943445b5
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2025-10-12 20:41:59 -0500

    fix panel control page

--- src/pages/OrderDatas.tsx
+++ src/pages/OrderDatas.tsx
@@ -23,86 +23,102 @@ import SingleProductSalesReport from "../components/orderDatas/SingleProductSale
 import UpperCategoryBasedSalesReport from "../components/orderDatas/UpperCategoryBasedSalesReport";
 import TabPanel from "../components/panelComponents/TabPanel/TabPanel";
 import { useGeneralContext } from "../context/General.context";
-import { useOrderContext } from "../context/Order.context";
 import { useUserContext } from "../context/User.context";
 import { OrderDataTabEnum } from "../types";
-import { useGetCategories } from "../utils/api/menu/category";
+import { useGetAllCategories } from "../utils/api/menu/category";
 import { useGetPanelControlPages } from "../utils/api/panelControl/page";
 
-export const OrderDataTabs = () => {
-  const categories = useGetCategories();
+export const OrderDataTabs = [
+  {
+    number: OrderDataTabEnum.DAILYINCOME,
+    label: "Daily Income",
+    icon: <TbReportMoney className="text-lg font-thin" />,
+    content: <DailyIncome />,
+    isDisabled: false,
+  },
+  {
+    number: OrderDataTabEnum.GROUPEDPRODUCTSALESREPORT,
+    label: "Product Sales",
+    icon: <HiOutlineDocumentReport className="text-lg font-thin" />,
+    content: <GroupedProductSalesReport />,
+    isDisabled: false,
+  },
+  {
+    number: OrderDataTabEnum.SINGLEPRODUCTSALESREPORT,
+    label: "Product Based Sales",
+    icon: <TbReportSearch className="text-lg font-thin" />,
+    content: <SingleProductSalesReport />,
+    isDisabled: false,
+  },
+  {
+    number: OrderDataTabEnum.UPPERCATEGORYBASEDSALESREPORT,
+    label: "Upper Category Based Sales",
+    icon: <TbCategoryPlus className="text-lg font-thin" />,
+    content: <UpperCategoryBasedSalesReport />,
+    isDisabled: false,
+  },
+  {
+    number: OrderDataTabEnum.CATEGORYBASEDSALESREPORT,
+    label: "Category Based Sales",
+    icon: <BiCategory className="text-lg font-thin" />,
+    content: <CategoryBasedSalesReport />,
+    isDisabled: false,
+  },
+  {
+    number: OrderDataTabEnum.DISCOUNTBASEDSALES,
+    label: "Discount Based Sales",
+    icon: <TbDiscount className="text-lg font-thin" />,
+    content: <DiscountBasedSales />,
+    isDisabled: false,
+  },
+  {
+    number: OrderDataTabEnum.COLLECTIONS,
+    label: "Collections",
+    icon: <SiFampay className="text-lg font-thin" />,
+    content: <Collections />,
+    isDisabled: false,
+  },
+  {
+    number: OrderDataTabEnum.ORDERS,
+    label: "Cafe Orders",
+    icon: <TbReportAnalytics className="text-lg font-thin" />,
+    content: <OrdersReport />,
+    isDisabled: false,
+  },
+  {
+    number: OrderDataTabEnum.IKASORDERS,
+    label: "Ikas Orders",
+    icon: <TbReportAnalytics className="text-lg font-thin" />,
+    content: <IkasOrders />,
+    isDisabled: false,
+  },
+  {
+    number: OrderDataTabEnum.PERSONALORDERDATAS,
+    label: "Personal Order Datas",
+    icon: <BsClipboard2Data className="text-lg font-thin" />,
+    content: <PersonalOrderDatas />,
+    isDisabled: false,
+  },
+];
+
+const OrderDatas = () => {
+  const {
+    setCurrentPage,
+    setSearchQuery,
+    orderDataActiveTab,
+    setOrderDataActiveTab,
+  } = useGeneralContext();
+  const categories = useGetAllCategories();
   const enumTabCount = Object.keys(OrderDataTabEnum).length / 2;
-  return [
-    {
-      number: OrderDataTabEnum.DAILYINCOME,
-      label: "Daily Income",
-      icon: <TbReportMoney className="text-lg font-thin" />,
-      content: <DailyIncome />,
-      isDisabled: false,
-    },
-    {
-      number: OrderDataTabEnum.GROUPEDPRODUCTSALESREPORT,
-      label: "Product Sales",
-      icon: <HiOutlineDocumentReport className="text-lg font-thin" />,
-      content: <GroupedProductSalesReport />,
-      isDisabled: false,
-    },
-    {
-      number: OrderDataTabEnum.SINGLEPRODUCTSALESREPORT,
-      label: "Product Based Sales",
-      icon: <TbReportSearch className="text-lg font-thin" />,
-      content: <SingleProductSalesReport />,
-      isDisabled: false,
-    },
-    {
-      number: OrderDataTabEnum.UPPERCATEGORYBASEDSALESREPORT,
-      label: "Upper Category Based Sales",
-      icon: <TbCategoryPlus className="text-lg font-thin" />,
-      content: <UpperCategoryBasedSalesReport />,
-      isDisabled: false,
-    },
-    {
-      number: OrderDataTabEnum.CATEGORYBASEDSALESREPORT,
-      label: "Category Based Sales",
-      icon: <BiCategory className="text-lg font-thin" />,
-      content: <CategoryBasedSalesReport />,
-      isDisabled: false,
-    },
-    {
-      number: OrderDataTabEnum.DISCOUNTBASEDSALES,
-      label: "Discount Based Sales",
-      icon: <TbDiscount className="text-lg font-thin" />,
-      content: <DiscountBasedSales />,
-      isDisabled: false,
-    },
-    {
-      number: OrderDataTabEnum.COLLECTIONS,
-      label: "Collections",
-      icon: <SiFampay className="text-lg font-thin" />,
-      content: <Collections />,
-      isDisabled: false,
-    },
-    {
-      number: OrderDataTabEnum.ORDERS,
-      label: "Cafe Orders",
-      icon: <TbReportAnalytics className="text-lg font-thin" />,
-      content: <OrdersReport />,
-      isDisabled: false,
-    },
-    {
-      number: OrderDataTabEnum.IKASORDERS,
-      label: "Ikas Orders",
-      icon: <TbReportAnalytics className="text-lg font-thin" />,
-      content: <IkasOrders />,
-      isDisabled: false,
-    },
-    {
-      number: OrderDataTabEnum.PERSONALORDERDATAS,
-      label: "Personal Order Datas",
-      icon: <BsClipboard2Data className="text-lg font-thin" />,
-      content: <PersonalOrderDatas />,
-      isDisabled: false,
-    },
+  const currentPageId = "order_datas";
+  const pages = useGetPanelControlPages();
+  const { user } = useUserContext();
+  if (!user || pages.length === 0) return <></>;
+  const currentPageTabs = pages.find(
+    (page) => page._id === currentPageId
+  )?.tabs;
+  const tabs = [
+    ...OrderDataTabs,
     ...(categories ?? [])
       .filter((cat) => cat?.isKitchenMenu)
       .map((category, index) => ({
@@ -117,26 +133,7 @@ export const OrderDataTabs = () => {
         isDisabled: false,
         kitchen: null,
       })),
-  ];
-};
-
-const OrderDatas = () => {
-  const {
-    setCurrentPage,
-    setSearchQuery,
-    orderDataActiveTab,
-    setOrderDataActiveTab,
-  } = useGeneralContext();
-  const { setFilterPanelFormElements, filterPanelFormElements } =
-    useOrderContext();
-  const currentPageId = "order_datas";
-  const pages = useGetPanelControlPages();
-  const { user } = useUserContext();
-  if (!user || pages.length === 0) return <></>;
-  const currentPageTabs = pages.find(
-    (page) => page._id === currentPageId
-  )?.tabs;
-  const tabs = OrderDataTabs().map((tab) => {
+  ].map((tab) => {
     return {
       ...tab,
       isDisabled: currentPageTabs
