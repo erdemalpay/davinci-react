ae652aa718c8209dac139d959fecb06c0c754bf6
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2025-10-21 15:57:53 +0300

    switchbuttondaki gereksiz ekleme kaldırıldı

--- src/components/panelComponents/common/SwitchButton.tsx
+++ src/components/panelComponents/common/SwitchButton.tsx
@@ -3,17 +3,15 @@ import { Switch } from "@headlessui/react";
 type Props = {
   checked: boolean;
   onChange: (value: (prev: boolean) => boolean) => void;
-  disabled?: boolean;
 };
 
-const SwitchButton = ({ checked, onChange, disabled = false }: Props) => {
+const SwitchButton = ({ checked, onChange }: Props) => {
   return (
     <Switch
       checked={checked}
       onChange={() => onChange((value) => !value)}
-      disabled={disabled}
       className={`${checked ? "bg-green-500" : "bg-red-500"}
-          relative inline-flex h-[20px] w-[36px] min-w-[36px] border-[1px] ${disabled ? "opacity-50 cursor-not-allowed" : "cursor-pointer"} rounded-full border-transparent transition-colors duration-200 ease-in-out focus:outline-none`}
+          relative inline-flex h-[20px] w-[36px] min-w-[36px] border-[1px] cursor-pointer rounded-full border-transparent transition-colors duration-200 ease-in-out focus:outline-none`}
     >
       <span
         aria-hidden="true"
