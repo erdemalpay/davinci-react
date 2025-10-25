201fc4fc890fec1c8471f9eb449145af3938e672
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2025-10-24 19:40:47 -0500

    games i know page is refactored

--- src/components/user/GamesIKnow.tsx
+++ src/components/user/GamesIKnow.tsx
@@ -1,5 +1,5 @@
 import { format } from "date-fns";
-import { useEffect, useState } from "react";
+import { useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useUserContext } from "../../context/User.context";
 import { Game, UserGameUpdateType } from "../../types";
@@ -26,7 +26,6 @@ const GamesIKnow = ({ userId }: Props) => {
   if (!panelUser) return <></>;
   const [learnDateModal, setLearnDateModal] = useState(false);
   const [isEnableEdit, setIsEnableEdit] = useState(false);
-  const [tableKey, setTableKey] = useState(0);
   const [
     isCloseAllConfirmationDialogOpen,
     setIsCloseAllConfirmationDialogOpen,
@@ -35,18 +34,27 @@ const GamesIKnow = ({ userId }: Props) => {
   const { updateUserGame } = updateUserGamesMutation();
   const user = useGetUserWithId(userId);
   const games = useGetGames();
-  const inputs = [
-    {
-      type: InputTypes.DATE,
-      formKey: "learnDate",
-      label: t("Learn Date"),
-      placeholder: t("Learn Date"),
-      required: true,
-    },
-  ];
-  const formKeys = [{ key: "learnDate", type: FormKeyTypeEnum.STRING }];
-  const columns =
-    user?._id === panelUser?._id && isEnableEdit
+
+  const inputs = useMemo(
+    () => [
+      {
+        type: InputTypes.DATE,
+        formKey: "learnDate",
+        label: t("Learn Date"),
+        placeholder: t("Learn Date"),
+        required: true,
+      },
+    ],
+    [t]
+  );
+
+  const formKeys = useMemo(
+    () => [{ key: "learnDate", type: FormKeyTypeEnum.STRING }],
+    []
+  );
+
+  const columns = useMemo(() => {
+    return user?._id === panelUser?._id && isEnableEdit
       ? [
           { key: t("Game"), isSortable: true },
           { key: t("Learn Date"), isSortable: true },
@@ -56,10 +64,14 @@ const GamesIKnow = ({ userId }: Props) => {
           { key: t("Game"), isSortable: true },
           { key: t("Learn Date"), isSortable: true },
         ];
+  }, [t, user, panelUser, isEnableEdit]);
 
-  const userGamesGameArray = user?.userGames.map((item) => item.game);
-  const rows =
-    user?._id === panelUser?._id
+  const userGamesGameArray = useMemo(() => {
+    return user?.userGames.map((item) => item.game);
+  }, [user]);
+
+  const rows = useMemo(() => {
+    return user?._id === panelUser?._id
       ? games.map((game) =>
           userGamesGameArray?.includes(game._id)
             ? {
@@ -78,26 +90,33 @@ const GamesIKnow = ({ userId }: Props) => {
             );
             return { ...game, learnDate: userGame?.learnDate };
           });
-  const rowKeys = [
-    {
-      key: "name",
-    },
-    {
-      key: "learnDate",
-      className: `min-w-32  `,
-      node: (row: Game) => {
-        const userGame = user?.userGames.find(
-          (userGame) => userGame.game === row._id
-        );
+  }, [user, panelUser, games, userGamesGameArray]);
 
-        return (
-          <p>
-            {userGame?.learnDate ? formatAsLocalDate(userGame.learnDate) : "-"}
-          </p>
-        );
+  const rowKeys = useMemo(
+    () => [
+      {
+        key: "name",
       },
-    },
-  ];
+      {
+        key: "learnDate",
+        className: `min-w-32  `,
+        node: (row: Game) => {
+          const userGame = user?.userGames.find(
+            (userGame) => userGame.game === row._id
+          );
+
+          return (
+            <p>
+              {userGame?.learnDate
+                ? formatAsLocalDate(userGame.learnDate)
+                : "-"}
+            </p>
+          );
+        },
+      },
+    ],
+    [user]
+  );
 
   function handleUpdateUserGame({
     gameId,
@@ -110,60 +129,65 @@ const GamesIKnow = ({ userId }: Props) => {
   }) {
     updateUserGame({ gameId, updateType, learnDate: learnDate });
   }
-  const filters = [
-    {
-      label: t("Enable Edit"),
-      isUpperSide: false,
-      node: (
-        <>
+
+  const filters = useMemo(
+    () => [
+      {
+        label: t("Enable Edit"),
+        isUpperSide: false,
+        node: (
+          <>
+            <CheckSwitch
+              checked={isEnableEdit}
+              onChange={() => setIsEnableEdit((value) => !value)}
+              checkedBg="bg-green-500"
+            ></CheckSwitch>
+          </>
+        ),
+      },
+    ],
+    [t, isEnableEdit]
+  );
+
+  const actions = useMemo(
+    () => [
+      {
+        name: t("Toggle Active"),
+        isModal: false,
+        isPath: false,
+        icon: null,
+        isDisabled: user?._id !== panelUser?._id || !isEnableEdit,
+        node: (row: Game) => (
           <CheckSwitch
-            checked={isEnableEdit}
-            onChange={() => setIsEnableEdit((value) => !value)}
-            checkedBg="bg-green-500"
+            checked={userGamesGameArray?.includes(row._id) ?? false}
+            onChange={() => {
+              if (userGamesGameArray?.includes(row._id)) {
+                setRowToAction(row);
+                setIsCloseAllConfirmationDialogOpen(true);
+              } else {
+                setLearnDateModal(true);
+                setRowToAction(row);
+              }
+            }}
           ></CheckSwitch>
-        </>
-      ),
-    },
-  ];
-  const actions = [
-    {
-      name: t("Toggle Active"),
-      isModal: false,
-      isPath: false,
-      icon: null,
-      isDisabled: user?._id !== panelUser?._id || !isEnableEdit,
-      node: (row: Game) => (
-        <CheckSwitch
-          checked={userGamesGameArray?.includes(row._id) ?? false}
-          onChange={() => {
-            if (userGamesGameArray?.includes(row._id)) {
-              setRowToAction(row);
-              setIsCloseAllConfirmationDialogOpen(true);
-            } else {
-              setLearnDateModal(true);
-              setRowToAction(row);
-            }
-          }}
-        ></CheckSwitch>
-      ),
-    },
-  ];
-  const filteredRows = () => {
+        ),
+      },
+    ],
+    [t, user, panelUser, isEnableEdit, userGamesGameArray]
+  );
+
+  const filteredRows = useMemo(() => {
     if (isEnableEdit) {
       return rows;
     }
     return rows.filter((row) => userGamesGameArray?.includes(row._id));
-  };
-  useEffect(() => {
-    setTableKey((prev) => prev + 1);
-  }, [user, isEnableEdit, games]);
+  }, [isEnableEdit, rows, userGamesGameArray]);
 
   return (
     <div className="w-full  h-fit">
       <GenericTable
-        key={tableKey}
         columns={columns}
-        rows={filteredRows()}
+        rows={filteredRows}
         rowKeys={rowKeys}
         actions={isEnableEdit ? actions : []}
         filters={user?._id === panelUser?._id ? filters : []}
@@ -173,7 +197,6 @@ const GamesIKnow = ({ userId }: Props) => {
       />
       {learnDateModal && rowToAction && (
         <GenericAddEditPanel
-          key={tableKey}
           isOpen={learnDateModal}
           close={() => setLearnDateModal(false)}
           inputs={inputs}
