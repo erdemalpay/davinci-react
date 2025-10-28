7b40d54e323b64b840797a19ee4c9d2ffe187487
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2025-10-28 13:28:24 -0500

    trying to fix merge error

--- src/context/Location.context.tsx
+++ src/context/Location.context.tsx
@@ -19,9 +19,10 @@ const LocationContext = createContext<LocationContextType>({
 
 export const LocationContextProvider = ({ children }: PropsWithChildren) => {
   const locationId = Number(localStorage.getItem(LOCATION_LOCAL_STORAGE_KEY));
-  const [selectedLocationId, setSelectedLocationId] = useState<number>(locationId || 1);
+  const [selectedLocationId, setSelectedLocationId] = useState<number>(
+    locationId || 1
+  );
   const locations = useGetStoreLocations();
-
   function selectLocation(locationId: number) {
     if (locationId !== -1) {
       localStorage.setItem(LOCATION_LOCAL_STORAGE_KEY, locationId.toString());
@@ -30,7 +31,6 @@ export const LocationContextProvider = ({ children }: PropsWithChildren) => {
     }
     setSelectedLocationId(locationId);
   }
-
   return (
     <LocationContext.Provider
       value={{
