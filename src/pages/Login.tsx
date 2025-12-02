1f3f4a51971fd49304a09cdfe93a3a860751b5ff
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2025-12-02 11:30:42 +0300

    login sayfası stilizasyonu-2

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { useLocation, useNavigate } from "react-router-dom";
+import { EyeIcon, EyeSlashIcon } from "@heroicons/react/24/outline";
 import { GenericButton } from "../components/common/GenericButton";
 import { RoleEnum } from "../types";
 import { LoginCredentials, useLogin } from "../utils/api/auth";
@@ -96,7 +97,7 @@ const Login = () => {
           onSubmit={handleSubmit}
         >
           <div className="flex flex-col">
-            <h2 className="text-5xl sm:text-6xl text-gray-800 dark:text-gray-100 leading-tight pt-8 font-bold">
+            <h2 className="text-5xl sm:text-6xl text-gray-800 dark:text-gray-100 leading-tight pt-8 font-bold text-center sm:text-left cursor-default select-none">
               Da Vinci Panel
             </h2>
           </div>
@@ -104,7 +105,7 @@ const Login = () => {
             <div className="flex flex-col mt-5">
               <label
                 htmlFor="username"
-                className="text-lg text-gray-800 dark:text-gray-100 leading-tight"
+                className="text-lg text-gray-800 dark:text-gray-100 leading-tight cursor-default select-none"
               >
                 {t("Username")}
               </label>
@@ -121,7 +122,7 @@ const Login = () => {
             <div className="flex flex-col mt-5">
               <label
                 htmlFor="password"
-                className="text-lg text-gray-800 dark:text-gray-100 fleading-tight"
+                className="text-lg text-gray-800 dark:text-gray-100 fleading-tight cursor-default select-none"
               >
                 {t("Password")}
               </label>
@@ -148,40 +149,9 @@ const Login = () => {
                   aria-label={showPassword ? "Hide password" : "Show password"}
                 >
                   {showPassword ? (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
-                      />
-                    </svg>
+                    <EyeSlashIcon className="w-5 h-5" />
                   ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      strokeWidth={1.5}
-                      stroke="currentColor"
-                      className="w-5 h-5"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
-                      />
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                      />
-                    </svg>
+                    <EyeIcon className="w-5 h-5" />
                   )}
                 </button>
               </div>
@@ -201,7 +171,7 @@ const Login = () => {
             <GenericButton
               type="submit"
               fullWidth
-              className="bg-gray-800 hover:bg-gray-800 px-8 py-3 text-sm transition-all duration-200 hover:shadow-lg hover:scale-[1.02]"
+              className="bg-gray-800 hover:bg-gray-800 px-8 py-3 text-l transition-all duration-200 hover:shadow-lg hover:scale-[1.02]"
             >
               {t("Login")}
             </GenericButton>
