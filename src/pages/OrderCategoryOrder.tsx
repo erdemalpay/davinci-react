3d457297b932b9a1ee1d1c0f45298ad3d3c3069e
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2025-12-14 15:05:01 +0300

    manü sayfasındaki order category order tabındaki header kaldırıldı

--- src/pages/OrderCategoryOrder.tsx
+++ src/pages/OrderCategoryOrder.tsx
@@ -1,6 +1,5 @@
 import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
-import { Header } from "../components/header/Header";
 import GenericTable from "../components/panelComponents/Tables/GenericTable";
 import { NO_IMAGE_URL } from "../navigation/constants";
 import { MenuCategory } from "../types";
@@ -47,7 +46,6 @@ const OrderCategoryOrder = () => {
 
   return (
     <>
-      <Header showLocationSelector={false} />
       <div className="w-[95%] mx-auto my-10 flex flex-col gap-6 ">
         <GenericTable
           rowKeys={rowKeys}
