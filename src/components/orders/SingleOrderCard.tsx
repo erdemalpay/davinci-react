fb51d58c15c3b17b04bed5a99df914dbc01926ea
Hilmi KALE <hilmi.kale@ogr.iu.edu.tr>
2025-12-24 15:53:09 +0300

    Reduce delay for order created sound playback in SingleOrderCard component

--- src/components/orders/SingleOrderCard.tsx
+++ src/components/orders/SingleOrderCard.tsx
@@ -65,7 +65,7 @@ const SingleOrderCard = ({ order, user }: Props) => {
         orderCreatedSound
           .play()
           .catch((error) => console.error("Error playing sound:", error));
-      }, 10000);
+      }, 5000);
     }
 
     return () => {
