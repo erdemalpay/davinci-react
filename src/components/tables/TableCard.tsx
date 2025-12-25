aff66b5de1a8cc6fc1624621e25bc7eb82ab1fa3
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2025-12-25 12:26:09 +0300

    oyuncu numara alanının yukarı taşınması

--- src/components/tables/TableCard.tsx
+++ src/components/tables/TableCard.tsx
@@ -347,6 +347,17 @@ export function TableCard({
 
   const orderInputs = useMemo(
     () => [
+
+      {
+        type: InputTypes.TEXT,
+        formKey: "activityPlayer",
+        label: t("Player Number"),
+        placeholder: t("Player Number"),
+        required: table?.type === TableTypes.ACTIVITY,
+        isDisabled: table?.type !== TableTypes.ACTIVITY,
+        isTopFlexRow: window.innerWidth <= 768,
+        isOnClearActive: true,
+      },
       {
         type: InputTypes.TAB,
         formKey: "category",
@@ -620,18 +631,8 @@ export function TableCard({
         isDisabled: table?.type !== TableTypes.ACTIVITY,
       },
       {
-        type: InputTypes.TEXT,
-        formKey: "activityPlayer",
-        label: t("Player Number"),
-        placeholder: t("Player Number"),
-        required: table?.type === TableTypes.ACTIVITY,
-        isDisabled: table?.type !== TableTypes.ACTIVITY,
-        isOnClearActive: true,
-      },
-      {
         type: InputTypes.TEXTAREA,
         formKey: "note",
-        label: t("Note"),
         placeholder: t("Note"),
         required: false,
         options:
