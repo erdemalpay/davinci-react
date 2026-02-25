dc9f3a58ac04dfa92e92a113b53318e98cfaf817
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2026-02-25 12:41:10 +0300

    kullanılmayan kodlar silindi

--- src/utils/isDisabledConditions.ts
+++ src/utils/isDisabledConditions.ts
@@ -1,23 +1,5 @@
 import { RoleEnum, User } from "../types";
 
-export const isDisabledConditionBrand = (user: User | null | undefined): boolean => {
-  return user
-    ? ![RoleEnum.MANAGER, RoleEnum.GAMEMANAGER, RoleEnum.OPERATIONSASISTANT].includes(user?.role?._id)
-    : true;
-};
-
-export const isDisabledConditionCountListMenu = (user: User | null | undefined): boolean => {
-  return user ? ![RoleEnum.MANAGER, RoleEnum.OPERATIONSASISTANT].includes(user?.role?._id) : true;
-};
-
-export const isDisabledConditionCountLists = (user: User | null | undefined): boolean => {
-  return user ? ![RoleEnum.MANAGER].includes(user?.role?._id) : true;
-};
-
-export const isDisabledConditionExpirationLists = (user: User | null | undefined): boolean => {
-  return user ? ![RoleEnum.MANAGER].includes(user?.role?._id) : true;
-};
-
 export const isDisabledConditionCreateNotification = (user: User | null | undefined): boolean => {
   return user ? ![RoleEnum.MANAGER].includes(user?.role?._id) : true;
 };
