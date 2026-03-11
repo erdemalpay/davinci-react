9385edf062d3aa76f725ac310043eadb7ec577dc
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2026-03-11 11:32:44 +0300

    modal kapatıldığında collectionId state'inin temizlenmesi

--- src/components/points/ConsumerPointHistory.tsx
+++ src/components/points/ConsumerPointHistory.tsx
@@ -388,6 +388,7 @@ const ConsumerPointHistoryComponent = () => {
         onClick={() => {
           setIsCollectionModalOpen(false);
           setSelectedTableId(undefined);
+          setSelectedCollectionId(undefined);
         }}
       >
         <div
@@ -497,6 +498,7 @@ const ConsumerPointHistoryComponent = () => {
               onClick={() => {
                 setIsCollectionModalOpen(false);
                 setSelectedTableId(undefined);
+                setSelectedCollectionId(undefined);
               }}
               className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
             >
