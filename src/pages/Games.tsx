0061faf2a4311dea84b945de4856c7d583758a1c
Osman Erdogan <114867365+osmansam@users.noreply.github.com>
2026-05-14 15:03:54 -0500

    fix games page tabs

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -6,7 +6,9 @@ import GamesTab from "../components/games/GamesTab";
 import { Header } from "../components/header/Header";
 import UnifiedTabPanel from "../components/panelComponents/TabPanel/UnifiedTabPanel";
 import { useGeneralContext } from "../context/General.context";
+import { useUserContext } from "../context/User.context";
 import { GamesPageTabEnum } from "../types";
+import { useGetPanelControlPages } from "../utils/api/panelControl/page";
 
 export const GamesPageTabs = [
   {
@@ -28,13 +30,29 @@ export const GamesPageTabs = [
 export default function Games() {
   const [activeTab, setActiveTab] = useState<number>(0);
   const { setCurrentPage, setSearchQuery } = useGeneralContext();
-
+  const currentPageId = "games";
+  const pages = useGetPanelControlPages();
+  const { user } = useUserContext();
+  if (!user || pages.length === 0) return <></>;
+  const currentPageTabs = pages.find(
+    (page) => page._id === currentPageId
+  )?.tabs;
+  const tabs = GamesPageTabs.map((tab) => {
+    return {
+      ...tab,
+      isDisabled: currentPageTabs
+        ?.find((item) => item.name === tab.label)
+        ?.permissionRoles?.includes(user.role._id)
+        ? false
+        : true,
+    };
+  });
   return (
     <>
       <Header showLocationSelector={false} />
       <div className="flex flex-col gap-2 mt-5">
         <UnifiedTabPanel
-          tabs={GamesPageTabs}
+          tabs={tabs}
           activeTab={activeTab}
           setActiveTab={setActiveTab}
           additionalOpenAction={() => {
