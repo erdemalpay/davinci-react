6b7b62afe7060887b5d4ef52a951e2df9917cecf
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2026-07-03 10:28:39 +0300

    Oyun atama penceresinde "test" kullanıcılarının gösterilmemesi

--- src/components/games/AssignGame.tsx
+++ src/components/games/AssignGame.tsx
@@ -66,6 +66,7 @@ const AssignGame = () => {
           )
           ?.filter(
             (user) =>
+              !user._id.startsWith("test_") &&
               user.userGames &&
               !user.userGames.find(
                 (userGameObject) => userGameObject.game === rowToAction?._id
