baf841a38b5e8c2f3dea21080eae95ee7aa743e1
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2026-07-17 13:34:10 +0300

    fix: useGetUserShifts yanlış location parametresi ve çeviri eksiklikleri düzeltildi

--- src/components/visits/VisitScheduleOverview.tsx
+++ src/components/visits/VisitScheduleOverview.tsx
@@ -165,8 +165,8 @@ const VisitScheduleOverview = () => {
   const columns = useMemo(
     () => [
       { key: t("User"), isSortable: true, correspondingKey: "userName" },
-      { key: "Part Time", isSortable: true, correspondingKey: "partTime" },
-      { key: "Full Time", isSortable: true, correspondingKey: "fullTime" },
+      { key: t("Part Time"), isSortable: true, correspondingKey: "partTime" },
+      { key: t("Full Time"), isSortable: true, correspondingKey: "fullTime" },
       {
         key: t("Wrong Entry"),
         isSortable: true,
