212fe440ebc79bf2d4323959930a894b61678eb3
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2026-07-21 14:27:34 +0300

    Gemini yorumu sonrası düzeltme

--- src/components/panelComponents/Tables/FilterPanel.tsx
+++ src/components/panelComponents/Tables/FilterPanel.tsx
@@ -65,7 +65,7 @@ const FilterPanel = <T,>({
     <div
       className={`flex flex-col gap-3 __className_a182b8 bg-white w-full min-w-full max-w-full ${
         isFilterPanelCoverTable ? "" : "sm:w-[20rem] sm:min-w-[20rem] sm:max-w-[20rem]"
-      } border h-fit pb-8 border-gray-200 rounded-md py-2 px-3 focus:outline-none overflow-hidden `}
+      } border h-fit pb-8 border-gray-200 rounded-md py-2 px-3 focus:outline-none `}
     >
       <div className="flex flex-row justify-between">
         <H4 className="my-1">{t("Filters")}</H4>
